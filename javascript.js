7e1464086ecaf81a93cfe131c0ee775859179e2b
Mikki Janower <65423058+mikki-janower@users.noreply.github.com>
2021-01-29 18:27:48 -0500

    init;

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+  $(document).on('mousemove', (event) => {
+  $('.colorblob').css({
+    left: event.clientX,
+    top: event.clientY,
+  });
+});
+
+ /* $('a').click(function() {
+  $('.colorblob').hide;
+  });*/
+
+
+//------------------closing bracket don't touch
+});
